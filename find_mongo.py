1fa3cb3dd4dac6bb94fb9f7a5a3af117f7e0978e
Hugo Cosme <hugutux@gmail.com>
2018-11-04 22:56:24 -0200

    metodos find e findOne

--- find_mongo.py
+++ find_mongo.py
@@ -1,3 +1,4 @@
+#referencia https://www.w3schools.com/python/python_mongodb_find.asp
 import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -5,12 +6,14 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["teste_pymongo"]
 mycol = mydb["Clientes"]
 
-###buscando um
+###buscando o primeiro da collection Clientes
 #x = mycol.find_one()
 #print(x)
 
-###buscando todos
-#for x in mycol.find():
-#    print(x)
+###buscando todos os documentos da collection Clientes
+for x in mycol.find():
+     print(x)
 
-#buscando um especifico
+###buscando um especifico na collection Clientes
+# for x in mycol.find({},{ "_id": 0, "nome": 1, "end": 1 }):
+#   print(x)
